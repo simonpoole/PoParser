39cad7eb00541663c8185ee00f195f8acabfaa4b
simonpoole <simon@poole.ch>
2022-06-02 15:45:03 +0200

    Fix some minor code issues

--- src/main/java/ch/poole/poparser/Po.java
+++ src/main/java/ch/poole/poparser/Po.java
@@ -9,87 +9,89 @@ import java.util.TreeSet;
 
 /**
  * Tiny wrapper around the parser
+ * 
  * @author Simon Poole
  *
  */
 public class Po implements Serializable {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private Map<String,HashMap<String,String>> m = null;
-	
-	/**
-	 * Parse the input stream in to a map
-	 * @param is
-	 * @throws ParseException
-	 */
-	public Po(InputStream is) throws ParseException {
-		PoParser pp = new PoParser(is);
-		m = pp.getMap();
-	}
-	
-	/**
-	 * Add strings where missing from a further file,
-	 * can be called multiple times.
-	 * @param is
-	 * @throws ParseException
-	 */
-	public void addFallback(InputStream is) throws ParseException {
-		PoParser pp = new PoParser(is);
-		Map<String,HashMap<String,String>> fallback = pp.getMap();
-		for (Entry<String,HashMap<String,String>> entry:m.entrySet()) { // loop over contexts
-		    String ctxt = entry.getKey();
-			Map<String,String> translations = entry.getValue();
-			Map<String,String> fallbackTranslations = fallback.get(ctxt);
-			if (fallbackTranslations == null) { // skip
-				continue;
-			}
-			for (String untranslated:new TreeSet<String>(translations.keySet())) { // shallow copy needed
-				if (translations.get(untranslated)==null) {
-					String fallbackTranslation = fallbackTranslations.get(untranslated);
-					if (fallbackTranslation!=null) {
-						translations.put(untranslated,fallbackTranslation);
-					}
-				}
-			}
-		}
-	}
-	
-	/**
-	 * Return the translation of a string or the string itself
-	 * if no translation exists.
-	 * @param id
-	 * @return
-	 */
-	public String t(String id) {
-		String r = m.get(null).get(id);
-		return r != null ? r : id;
-	}
 
-	/**
-	 * Return the translation of a string or the string itself
-	 * if no translation exists, considering context.
-	 * @param id
-	 * @return
-	 */
-	public String t(String ctxt, String id) {
-		Map<String, String> mctxt = m.get(ctxt);
-		if (mctxt != null) {
-			String r = mctxt.get(id);
-			return r != null ? r : id;
-		} 
-		return id;
-	}
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
-	
-	/**
-	 * Return the full map 
-	 * @return a map of maps (one per context) of the source and translations strings
-	 */
-	public Map<String,HashMap<String,String>> getMap() {
-		return m;
-	}
+    private Map<String, Map<String, String>> m = null;
+
+    /**
+     * Parse the input stream in to a map
+     * 
+     * @param is
+     * @throws ParseException
+     */
+    public Po(InputStream is) throws ParseException {
+        PoParser pp = new PoParser(is);
+        m = pp.getMap();
+    }
+
+    /**
+     * Add strings where missing from a further file, can be called multiple times.
+     * 
+     * @param is
+     * @throws ParseException
+     */
+    public void addFallback(InputStream is) throws ParseException {
+        PoParser pp = new PoParser(is);
+        Map<String, Map<String, String>> fallback = pp.getMap();
+        for (Entry<String, Map<String, String>> entry : m.entrySet()) { // loop over contexts
+            String ctxt = entry.getKey();
+            Map<String, String> translations = entry.getValue();
+            Map<String, String> fallbackTranslations = fallback.get(ctxt);
+            if (fallbackTranslations == null) { // skip
+                continue;
+            }
+            for (String untranslated : new TreeSet<String>(translations.keySet())) { // shallow copy needed
+                if (translations.get(untranslated) == null) {
+                    String fallbackTranslation = fallbackTranslations.get(untranslated);
+                    if (fallbackTranslation != null) {
+                        translations.put(untranslated, fallbackTranslation);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Return the translation of a string or the string itself if no translation exists.
+     * 
+     * @param id
+     * @return
+     */
+    public String t(String id) {
+        String r = m.get(null).get(id);
+        return r != null ? r : id;
+    }
+
+    /**
+     * Return the translation of a string or the string itself if no translation exists, considering context.
+     * 
+     * @param id
+     * @return
+     */
+    public String t(String ctxt, String id) {
+        Map<String, String> mctxt = m.get(ctxt);
+        if (mctxt != null) {
+            String r = mctxt.get(id);
+            return r != null ? r : id;
+        }
+        return id;
+    }
+
+    /**
+     * Return the full map
+     * 
+     * @return a map of maps (one per context) of the source and translations strings
+     */
+    public Map<String, Map<String, String>> getMap() {
+        return m;
+    }
 }
--- src/test/java/ch/poole/poparser/PoParserTest.java
+++ src/test/java/ch/poole/poparser/PoParserTest.java
@@ -18,95 +18,88 @@ import org.junit.Test;
 
 /**
  * Couple of tests for PoParser
+ * 
  * @author Simon Poole
  *
  */
 public class PoParserTest {
 
-	@Test
-  	public void regressionTest() 
-  	{
-		String poFile = "testinput/preset_tr.po";
-		String resultsFile = poFile + "-results";
-		BufferedWriter bwResults = null;
-		BufferedReader brResults = null;
-		String r = null;
-		String expected = null;
-		try
-    	{ 
-    		Po po = new Po(new FileInputStream(poFile));
-    		
-			try {
-				brResults = new BufferedReader(new InputStreamReader(new FileInputStream(resultsFile), "UTF8"));
-			} catch (FileNotFoundException fnfex) {
-				System.out.println("File not found " + fnfex.toString()); // don't fail
-			} catch (UnsupportedEncodingException ueex) {
-				fail(ueex.toString());
-			}
-			try {
-				bwResults = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(resultsFile +"-temp"), "UTF8"));
-			} catch (UnsupportedEncodingException ueex) {
-				fail(ueex.toString());
-			}
+    @Test
+    public void regressionTest() {
+        String poFile = "testinput/preset_tr.po";
+        String resultsFile = poFile + "-results";
+        BufferedWriter bwResults = null;
+        BufferedReader brResults = null;
+        String r = null;
+        String expected = null;
+        try {
+            Po po = new Po(new FileInputStream(poFile));
 
-      		try
-      		{
-       			Map<String,HashMap<String,String>> m = po.getMap();
-       			for (String k:m.keySet()) {
-       				for (String s:m.get(k).keySet()) {
-       					r = po.t(s);
-       					bwResults.write(encodeNewlines(r)+"\n");
-       					if (brResults != null) {
-       						expected = decodeNewlines(brResults.readLine());
-       						assertEquals(r,expected);
-       					}
-       					r = po.t("test",s);
-       					bwResults.write(encodeNewlines(r)+"\n");
-       					if (brResults != null) {
-       						expected = decodeNewlines(brResults.readLine());
-       						assertEquals(r,expected);
-       					}
-       				}
-       			}
-      		}
-      		catch (Exception e)
-      		{
-      			fail("Caught an exception " + e.getMessage());
-      		}
-      		catch (Error e)
-      		{
-      			fail("Caught an error " + e.getMessage());
-      		} 	
-    	} catch (FileNotFoundException fnfex)
-    	{
-      		fail("File not found " + fnfex.toString());
-    	} catch (ParseException pex) {
-    		fail(pex.toString());
-			pex.printStackTrace();
-		} catch (AssertionError ae) {
-			System.out.println("Assertion failed for (expected) >" + expected);
-			System.out.println("vs                        (got) >" + r);
-			throw ae;
-		} finally {
-			try {
-				if (bwResults != null) {
-					bwResults.close();
-				}
-				if (brResults != null) {
-					brResults.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-  	}
+            try {
+                brResults = new BufferedReader(new InputStreamReader(new FileInputStream(resultsFile), "UTF8"));
+            } catch (FileNotFoundException fnfex) {
+                System.out.println("File not found " + fnfex.toString()); // don't fail
+            } catch (UnsupportedEncodingException ueex) {
+                fail(ueex.toString());
+            }
+            try {
+                bwResults = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(resultsFile + "-temp"), "UTF8"));
+            } catch (UnsupportedEncodingException ueex) {
+                fail(ueex.toString());
+            }
 
-	private String encodeNewlines(String r) {
-		return r.replace((CharSequence)"\n", (CharSequence)"\\n"); 
-	}
-	
-	private String decodeNewlines(String r) {
-		return r.replace((CharSequence)"\\n", "\n"); 
-	}
+            try {
+                Map<String, Map<String, String>> m = po.getMap();
+                for (String k : m.keySet()) {
+                    for (String s : m.get(k).keySet()) {
+                        r = po.t(s);
+                        bwResults.write(encodeNewlines(r) + "\n");
+                        if (brResults != null) {
+                            expected = decodeNewlines(brResults.readLine());
+                            assertEquals(r, expected);
+                        }
+                        r = po.t("test", s);
+                        bwResults.write(encodeNewlines(r) + "\n");
+                        if (brResults != null) {
+                            expected = decodeNewlines(brResults.readLine());
+                            assertEquals(r, expected);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                fail("Caught an exception " + e.getMessage());
+            } catch (Error e) {
+                fail("Caught an error " + e.getMessage());
+            }
+        } catch (FileNotFoundException fnfex) {
+            fail("File not found " + fnfex.toString());
+        } catch (ParseException pex) {
+            fail(pex.toString());
+            pex.printStackTrace();
+        } catch (AssertionError ae) {
+            System.out.println("Assertion failed for (expected) >" + expected);
+            System.out.println("vs                        (got) >" + r);
+            throw ae;
+        } finally {
+            try {
+                if (bwResults != null) {
+                    bwResults.close();
+                }
+                if (brResults != null) {
+                    brResults.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String encodeNewlines(String r) {
+        return r.replace((CharSequence) "\n", (CharSequence) "\\n");
+    }
+
+    private String decodeNewlines(String r) {
+        return r.replace((CharSequence) "\\n", "\n");
+    }
 
 }
